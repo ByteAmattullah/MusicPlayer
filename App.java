301f7f1e6c8f29a457402b2cfa7445c7e9a98bed
ByteAmattullah <amattullah.elma.232@northsouth.edu>
2024-11-04 12:03:54 +0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,1 +1,17 @@
+import javax.swing.*;
 
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+                LaunchPage launch= new LaunchPage();
+
+
+//                Song song = new Song("src/assets/Wind Riders - Asher Fulero.mp3");
+//                System.out.println(song.getSongTitle());
+//                System.out.println(song.getSongArtist());
+            }
+        });
+    }
+}
